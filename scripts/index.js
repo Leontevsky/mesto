df7be33b1c2b14fc28abf250e38e8956b92cd5ac
Leontevsky <alexander.leontevsky@gmail.com>
2021-02-25 02:34:17 +0300

    all done

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,17 @@
-let showPopupButton = document.querySelector('#show-popup');
 let popup = document.querySelector('.popup');
+let showPopupButton = document.querySelector('#show-popup');
 let closePopupButton = document.querySelector('.popup__close');
+let nameInput = document.querySelector('#name');
+let jobInput = document.querySelector('#job');
+let formElement = document.querySelector('.popup__form');
 
  function showPopup() {
     popup.classList.add('popup_open');
  }
- 
 
+document.querySelector('#name').value = document.querySelector('.profile__title').textContent;
+document.querySelector('#job').value = document.querySelector('.profile__subtitle').textContent;
+ 
  function closePopup() {
      popup.classList.remove('popup_open');
  }
@@ -14,18 +19,26 @@ let closePopupButton = document.querySelector('.popup__close');
  showPopupButton.addEventListener('click', showPopup);
  closePopupButton.addEventListener('click', closePopup);
 
-
- let formElement = document.querySelector('.popup__form');
- let nameInput = document.querySelector('#name');
- let jobInput = document.querySelector('#job');
-
-
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
-    document.querySelector('.profile__title') = nameInput.value; 
-    document.querySelector('.profile__subtitle') = jobInput.value; 
+    document.querySelector('.profile__title').textContent = nameInput.value; 
+    document.querySelector('.profile__subtitle').textContent = jobInput.value;
+
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
 
-closePopup();
+// 2 вариант открыть или закрыть Popup через toggle
+
+// function togglePopup() {
+//     if (popup.classList.contains('popup_open')) {
+//         console.log('Нажали на крестик');
+//     }
+
+//     popup.classList.toggle('popup_open');
+// }
+
+// showPopupButton.addEventListener('click', togglePopup);
+// closePopupButton.addEventListener('click', togglePopup);
+
