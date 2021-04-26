a88e23a24250a5ce30416985a81fc282ddd1b142
Alexander Leontevsky <alexander.leontevsky@gmail.com>
2021-04-26 05:43:41 +0300

    superhotfix

---
+++ src/scripts/index.js
@@ -0,0 +1,92 @@
+import {
+    initialCards,
+    showEditFormButton,
+    editFormPopup,
+    popupNew,
+    showCreateFormButton,
+    nameInput,
+    jobInput,
+    enableValidation,
+    templateElement,
+} from '../scripts/constants.js'
+import Card from './Card.js';
+import { FormValidator } from './FormValidator.js'
+import PopupWithImage from '../components/PopupWithImage.js'
+import PopupWithForm from '../components/PopupWithForm.js'
+import Section from '../components/Section.js'
+import UserInfo from '../components/UserInfo.js'
+import '../pages/index.css';
+
+function createCard(name, link, cardElement) {
+    const card = new Card(name, link, cardElement, () => { imagePopup.open({ name, link }) });
+    return card
+}
+
+// Рендер
+const renderCardList = new Section({
+    items: initialCards, // массив данных на основе которого надо рисовать карточки
+    renderer: function(item) { // функция, которая нужна для отрисовки одной карточки
+        const card = createCard(item.name, item.link, '.template', () => {
+            imagePopup.open(item);
+        })
+        const cardNew = card.generateCard()
+        renderCardList.addItem(cardNew)
+
+    }
+}, '.elements__list')
+renderCardList.rendererItems()
+
+//Попап с картинкой
+const imagePopup = new PopupWithImage('.popup_type_image');
+imagePopup.setEventListeners()
+
+const userInfo = new UserInfo('.profile__title', '.profile__subtitle')
+
+//Попап редактирования профиля
+const userInfoPopup = new PopupWithForm(
+    '.popup_type_edit',
+    (values) => {
+        console.log(values)
+        const item = { name: values.name, job: values.job };
+        userInfo.setUserInfo(item.name, item.job);
+
+        userInfoPopup.close()
+
+    }
+)
+userInfoPopup.setEventListeners()
+
+//Попап создания новой карточки
+const newCardPopup = new PopupWithForm('.popup_type_new',
+    (values) => {
+        console.log(values)
+        const item = { name: values.place, link: values.link }
+        const newElement = createCard(item.name, item.link, templateElement)
+        const newCard = newElement.generateCard()
+        renderCardList.addItem(newCard)
+        newCardPopup.close()
+    }
+)
+
+newCardPopup.setEventListeners()
+
+showCreateFormButton.addEventListener('click', () => {
+    newCardPopup.open();
+});
+
+showEditFormButton.addEventListener('click', () => {
+    const allInfo = userInfo.getUserInfo();
+    nameInput.value = allInfo.name;
+    jobInput.value = allInfo.job;
+    userInfoPopup.open()
+})
+
+showCreateFormButton.addEventListener('click', () => {
+    newCardPopup.open()
+})
+
+const editFormValidator = new FormValidator(enableValidation, editFormPopup);
+const cardFormValidator = new FormValidator(enableValidation, popupNew);
+
+editFormValidator.enableValidation();
+cardFormValidator.enableValidation();
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+module.exports = {
+    entry: './src/scripts/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+            test: /\.css$/i,
+            use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                    },
+                },
+                'postcss-loader'
+            ],
+        }, {
+            test: /\.(png|jpe?g|gif|svg|woff2)$/i,
+            use: [{
+                loader: 'file-loader',
+                options: { esModule: false }
+            }, ],
+        }, ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+};
