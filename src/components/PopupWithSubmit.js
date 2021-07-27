8c16db9d1b334076f62fc047ead54a768b337109
Alexander Leontevsky <alexander.leontevsky@gmail.com>
2021-07-27 14:58:10 +0300

    Проверка

---
+++ src/components/PopupWithSubmit.js
@@ -0,0 +1,21 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithSubmit extends Popup {
+  constructor(popupSelector, handleSubmit) {
+    super(popupSelector);
+    this._handleSubmit = handleSubmit;
+    this._popupForm = this._popup.querySelector(".popup__form");
+  }
+  setEventListeners() {
+    //на форму вешаю слушатель сабмита, затем использую коллбэк который использует внутри себя карточку, которую мы кликнем удалять
+    this._popupForm.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+      this._handleSubmit(this._card);
+    });
+  }
+  open(card) {
+    //типо инциализируем карточку это для слушателя, чтобы потом оно могло ее открыть
+    this._card = card;
+    super.open();
+  }
+}
--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,44 +1,50 @@
-export const initialCards = [{
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+export const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
 ];
 
-export const showEditFormButton = document.querySelector('#show-popup')
-export const editFormPopup = document.querySelector('.popup_type_edit')
-export const popupNew = document.querySelector('.popup_type_new')
-export const showCreateFormButton = document.querySelector('#show-popup-new')
-export const nameInput = document.querySelector('#name')
-export const jobInput = document.querySelector('#job')
-export const templateElement = document.querySelector('.template')
+export const showEditFormButton = document.querySelector("#show-popup");
+export const editFormPopup = document.querySelector(".popup_type_edit");
+export const popupNew = document.querySelector(".popup_type_new");
+export const showCreateFormButton = document.querySelector("#show-popup-new");
+export const showUserAvatarButton = document.querySelector(
+  ".profile__container"
+);
+export const newAvatarPopup = document.querySelector(".popup_type_update");
+
+export const nameInput = document.querySelector("#name");
+export const jobInput = document.querySelector("#job");
+export const templateElement = document.querySelector(".template");
 
 export const popupUserPicSelector = ".popup_update";
 
 export const enableValidation = {
-    formSelector: '.popup__form',
-    inputSelector: '.popup__input',
-    submitButtonSelector: '.popup__button',
-    inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
-}
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".popup__button",
+  inactiveButtonClass: "popup__button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
